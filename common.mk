ad02a27d7366ff0a045693a9c84daa8352aa0957
tzhongyan <iam@tzhongyan.com>
2021-04-29 17:07:46 +0800

    Builds but not work
    - Crashes on start, will need to check what is missing

--- common.mk
+++ common.mk
@@ -1,6 +1,4 @@
 src_ver := -D'GIT_COMMIT="$(shell git rev-parse --short HEAD)"'
 bindir = $(top_srcdir)/output
 
-AM_CXX_FLAGS = -I$(top_srcdir)/lib/orca
-AM_C_FLAGS = -I$(top_srcdir)/lib/orca
-AM_LD_ADD = $(top_srcdir)/lib/orca/lib
+AM_CFLAGS = -I$(top_srcdir)/lib -I$(top_srcdir)/lib/orca/common -I$(top_srcdir)/lib/orca/common/third-party ${libcurl_LIBS}
--- src/crypto.c
+++ src/crypto.c
@@ -1,11 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include "orca/discord.h"
 
 #define UNUSED(x) (void)(x)
 
+void on_message_create(
+    struct discord *client,
+    const struct discord_user *bot,
+    const struct discord_message *msg)
+{
+    // make sure bot doesn't echoes other bots
+    if (msg->author->bot)
+    return;
+
+    struct discord_create_message_params params = {
+        .content = msg->content
+    };
+
+    struct discord_message_reference msg_ref;
+    discord_message_reference_init(&msg_ref);
+    if(msg->referenced_message) {
+        msg_ref.message_id = msg->referenced_message->id;
+        msg_ref.channel_id = msg->channel_id;
+        msg_ref.guild_id = msg->guild_id;
+
+        params.message_reference = &msg_ref;
+    }
+
+    discord_create_message(client, msg->channel_id, &params, NULL);
+}
+
 int main(int argc, char** argv)
 {
-    UNUSED(argc);
-    UNUSED(argv);
+    const char *config_file = NULL;
+    struct discord *client = NULL;
 
+    if (argc > 1) {
+        config_file = argv[1];
+    }
+    else {
+        config_file = "bot.config";
+    }
+
+    discord_global_init();
+
+    client = discord_config_init(config_file);
+    assert(NULL != client);
+
+    discord_set_on_message_create(client, &on_message_create);
+    discord_run(client);
+
+    discord_cleanup(client);
+
+    discord_global_cleanup();
     return 0;
 }
