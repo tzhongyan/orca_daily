07371194a1e6a15d8385132bddf70e8a8b9066fb
tzhongyan <iam@tzhongyan.com>
2021-04-29 18:09:08 +0800

    Using UpperCammelCase for structs

--- src/crypto.c
+++ src/crypto.c
@@ -2,16 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+// #include <signal.h>
 
 #include "orca/discord.h"
+#include "crypto.h"
 
 #define UNUSED(x) (void)(x)
 
+static Discord *client = NULL;
+
 void on_message_create(
-    struct discord *client,
-    const struct discord_user *bot,
-    const struct discord_message *msg)
+    Discord *client,
+    const DiscordUser *bot,
+    const DiscordMessage *msg)
 {
+    UNUSED(bot);
+
     // make sure bot doesn't echoes other bots
     if (msg->author->bot)
     return;
@@ -20,7 +26,7 @@ void on_message_create(
         .content = msg->content
     };
 
-    struct discord_message_reference msg_ref;
+    DiscordMessageReference msg_ref;
     discord_message_reference_init(&msg_ref);
     if(msg->referenced_message) {
         msg_ref.message_id = msg->referenced_message->id;
@@ -33,10 +39,53 @@ void on_message_create(
     discord_create_message(client, msg->channel_id, &params, NULL);
 }
 
+void startup_message(Discord *client, const DiscordUser* bot)
+{
+    UNUSED(bot);
+    struct discord_create_message_params params = {
+        .content = "Hello!"
+    };
+    discord_create_message(client, msg->channel_id, &params, NULL);
+}
+
+
+// static void signal_handler(int signum)
+// {
+//     switch (signum) {
+//         case SIGINT:
+//         case SIGTERM:
+//             discord_gateway_shutdown(client);
+//             // exit(0);
+//             break;
+//         case SIGHUP:
+//             /* TODO: reset logfile */
+//             break;
+//         case SIGQUIT:
+//             exit(127);
+//            break;
+//         default:
+//             /* Ignore */
+//             break;
+//     }
+// }
+
+// static void setup_signal_handlers(void)
+// {
+//     struct sigaction act;
+
+//     act.sa_handler = signal_handler;
+//     sigemptyset(&act.sa_mask);
+//     act.sa_flags = 0;
+//     sigaction(SIGINT, &act, NULL);
+//     sigaction(SIGTERM, &act, NULL);
+//     sigaction(SIGQUIT, &act, NULL);
+//     sigaction(SIGHUP, &act, NULL);
+//     sigaction(SIGPIPE, &act, NULL);
+// }
+
 int main(int argc, char** argv)
 {
     const char *config_file = NULL;
-    struct discord *client = NULL;
 
     if (argc > 1) {
         config_file = argv[1];
@@ -45,12 +94,17 @@ int main(int argc, char** argv)
         config_file = "bot.config";
     }
 
+    /* Setup sigact */
+    // setup_signal_handlers();
+
     discord_global_init();
 
     client = discord_config_init(config_file);
     assert(NULL != client);
 
     discord_set_on_message_create(client, &on_message_create);
+    discord_set_on_ready(client, &startup_message);
+
     discord_run(client);
 
     discord_cleanup(client);
---
+++ src/crypto.h
@@ -0,0 +1,14 @@
+/**
+ * Changing struct snake_case into UpperCammelCase.
+ * More like Python heh.
+ */
+#ifndef CRYPTO_H
+#define CRYPTO_H
+#include "orca/discord.h"
+
+typedef struct discord Discord;
+typedef struct discord_user DiscordUser;
+typedef struct discord_message DiscordMessage;
+typedef struct discord_message_reference DiscordMessageReference;
+
+#endif /* CRYPTO_H */
